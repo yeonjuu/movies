7c59d2ab5af34694325410266b3d98e7b40a9aba
yeonjuu <yeonju9896@gmail.com>
2025-06-23 02:04:28 +0900

    Add global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { theme } from './styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
